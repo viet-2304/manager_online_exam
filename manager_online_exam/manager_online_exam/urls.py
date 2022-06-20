5e438c0e523534c98eb350bc3b4a69dcbe04444e
levanduandut <77284378+levanduandut@users.noreply.github.com>
2022-06-20 17:41:22 +0700

    Urls and View Student

--- manager_online_exam/manager_online_exam/urls.py
+++ manager_online_exam/manager_online_exam/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path,include
 from django.contrib import admin
-from django.urls import path
+from exam import views
+from django.contrib.auth.views import LogoutView,LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
